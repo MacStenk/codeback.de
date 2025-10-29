f8f22eefc88c4c198d45cf3a178dbc2de78a5e66
Steven Noack <MacStenk@users.noreply.github.com>
2025-10-29 04:37:50 +0100

    FIX: Use TINA_CLIENT_ID environment variable for production

--- tina/config.ts
+++ tina/config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   branch,
 
   // Deine TinaCloud Client ID
-  clientId: process.env.TINA_CLIENT_ID,
+  clientId: "c2b183f6-e2c7-495c-aee1-7cdcf967ecfa",
   token: process.env.TINA_TOKEN,
 
   build: {
